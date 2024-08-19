f55d1b3f216c6b8130c037bf7c8cd5098fc542f3
Vishaylinp <vishaylinparshotam@gmail.com>
2024-08-19 12:36:44 +0000

    back to normal

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -34,7 +34,7 @@ class Server:
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
         start_point, end_point = index_range(page, page_size)
-        data = self.__dataset()
+        data = self.dataset()
         if start_point > len(data):
             return []
         return data[start_point:end_point]
