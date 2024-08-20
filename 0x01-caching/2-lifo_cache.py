2732f49cbc63cfc481fd063d04bfea1f7f538907
Vishaylinp <vishaylinparshotam@gmail.com>
2024-08-20 13:17:13 +0000

    lifO

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""LIFO"""
+from collections import OrderedDict
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """last in first out"""
+
+    def __init__(self):
+        """initilisation"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """add item"""
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                last, _ = self.cache_data.popitem(True)
+                print("DISCARD:", last)
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
+
+    def get(self, key):
+        """retrieve"""
+        return self.cache_data(key, None)
