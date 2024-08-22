2bbef0e160a467acc37d16be515e38fe2c38af6c
Vishaylinp <vishaylinparshotam@gmail.com>
2024-08-22 14:56:07 +0000

    least recently used

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Least Recently used caching method
+"""
+from collections import OrderedDict
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """Class for Least recently used
+    """
+
+    def __init__(self):
+        """initialisation
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Add items
+        """
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                lru, _ = self.cache_data.popitem(True)
+                print("DISCARD:", lru)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieve item
+        """
+        return self.cache_data.get(key, None)
