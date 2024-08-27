19a20d438219abc3156130e4d662f904661415f0
Vishaylinp <vishaylinparshotam@gmail.com>
2024-08-27 13:29:14 +0000

    pycode error

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
-"""Flask app"""
+"""Basic Flask app"""
 from flask import Flask, render_template
 from flask_babel import Babel
+
 
 class Config:
     """config class"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
-    
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
+
 
 @app.route("/")
 def get_index_page():
