3fcbdb2151f967fbb3c9d3dc3124264b71ff205e
Vishaylinp <vishaylinparshotam@gmail.com>
2024-08-27 13:45:16 +0000

    get locale function

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
@@ -15,6 +15,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
+
 @babel.localeselector
 def get_locale():
     """get locale"""
@@ -24,7 +25,7 @@ def get_locale():
 @app.route("/")
 def get_index_page():
     """render template"""
-    return render_template("1-index.html")
+    return render_template("2-index.html")
 
 
 if __name__ == "__main__":
